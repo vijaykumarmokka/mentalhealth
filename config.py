be7363d000334556ac24195d5e9f9097396f9546
nikhil sopala <gireeshkumarsurampalli@my.unt.edu>
2024-11-03 00:43:13 +0530

    Initial commit for Azure deployment

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # For demo purposes, SQLite is used here
+    
+    # Azure SQL Database connection string
+    SQLALCHEMY_DATABASE_URI = (
+        'mssql+pyodbc://Cs4hmhiHGrT%24cwmb:helloword-server@helloword.database.windows.net/helloword-database?driver=ODBC+Driver+17+for+SQL+Server'
+    )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
